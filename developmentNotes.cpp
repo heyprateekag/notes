d10f6edadf45a3a26237cde6573f782d2048a2f3
Prateek.Agarwal-ext <prateek.agarwal-ext@bshg.com>
2022-04-28 17:53:25 +0530

    update

--- developmentNotes.cpp
+++ developmentNotes.cpp
@@ -37,3 +37,30 @@ add this constructor in AppModule class
 now terminate the terminal and run ng serve again
 then use font-awesome icons with this tag
 <fa-icon [icon]="['fas', '<icon-name>']"></fa-icon>
+
+* mouse pressed and mouse release event
+<span (mousedown)="functionName()" (mouseup)="functionName()">show</span>
+
+* this.jobTitles = data.filter((val) => val !== null); //for removing a particular entry from array
+
+
+*how to limit *ngFor upto particular index
+  *ngFor="let que of details| slice:0:3"
+  //this will only traverse from index 0, 1, 2
+
+
+*how to make a div footer of its parent div
+
+.parent-div {
+  position: relative
+}
+
+.footer-div {
+  position: absolute,
+  bottom: 0px
+}
+
+isNan function returns true if the argument is not a number; otherwise, it is false.
+
+
+'this' keyword refers to the object from where it was called.
